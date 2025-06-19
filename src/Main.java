9fd1669443d2c4555dd99173b7bd18ed1f6da7b3
Mikell <89644541+Kywll@users.noreply.github.com>
2025-06-19 19:47:31 +0800

    Update Main.java
    Looped player's input to actually let them play the game. Hid the tiles content until it's revealed. Display the board on each input to see the changes. Made a displayBoard function which displays the board and playerInput function to ask for player input and make the tile's isClicked variable to true

--- src/Main.java
+++ src/Main.java
@@ -2,63 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
+
+
 public class Main {
-    public static void main(String[] args){
-        //Thought Process
-        //Make a Game Board where you can detect surroundings
-        //Have Random Bombs
-        //Have a number on a tile based on how many bombs nearby
-        //Tile with revealed and unrevealed state
-
-        // June 19, 2025 To do:
-        // Hide it all first then only show the number of surrounding bombs near it after clicking
-
-
-
-
-        Tile[] tiles = new Tile[25];
-
-        Tile[][] board = new Tile[5][5];
-
-        Random random = new Random();
-        int number;
-        int bombLimit = 5;
-        int bombNumber = 0;
-
-
-        int num = 0;
-
-        //Make the board
-        for(int i=0;i<5;i++){
-            for (int j =0;j<5;j++){
-                number = random.nextInt(0, 2);
-
-                if (number == 1 && bombNumber < bombLimit){
-                    tiles[num] = new Tile(num, false, "Bomb");
-                    bombNumber++;
-                }
-                else{
-                    tiles[num] = new Tile(num, false, "Normal");
-
-
-                }
-
-                board[i][j] = tiles[num];
-
-                //System.out.println(board[i][j]);
-
-                num = num + 1;
-
-
-
-
-                // maybe make it 2d array like do Tiles[j][i]
-
-
-            }
-            System.out.println("");
-        }
-
+    public static void displayBoard(Tile[][] board){
         //Display Board
         for(int i=0;i<5;i++){
             for (int j =0;j<5;j++){
@@ -120,16 +67,18 @@ public class Main {
                 catch (Exception ArrayIndexOutOfBoundsException) {
                 }
 
-
-                if (board[i][j].tileType == "Bomb"){
-                    System.out.print("@");
-                }
-                else if (surroundingBombs == 0) {
+                if (board[i][j].isClicked){
+                    if (board[i][j].tileType == "Bomb"){
+                        System.out.print("@");
+                    } else if (surroundingBombs == 0) {
+                        System.out.print("0");
+                    } else if (surroundingBombs != 0) {
+                        System.out.print(surroundingBombs);
+                    }
+                } else {
                     System.out.print("=");
-                } else if (surroundingBombs != 0) {
-                    System.out.print(surroundingBombs);
-                    surroundingBombs = 0;
                 }
+                surroundingBombs = 0;
 
                 // Displays the index of bombs
                 //System.out.println("["+i+j+"}");
@@ -137,10 +86,9 @@ public class Main {
             }
             System.out.println("");
         }
-        for (Tile t : tiles){
-            t.displayInfo();
-        }
+    }
 
+    public static void playerInput(Tile[][] board){
         Scanner input = new Scanner(System.in);
         System.out.println("Pick 1-25:");
         int choice = input.nextInt();
@@ -148,85 +96,171 @@ public class Main {
         switch (choice) {
             case 1:
                 board[0][0].isRevealed();
-                System.out.println(board[0][0].isClicked);
+                board[0][0].isClicked = true;
                 break;
             case 2:
-                System.out.println(board[0][1]);
+                board[0][1].isRevealed();
+                board[0][1].isClicked = true;
                 break;
             case 3:
-                System.out.println(board[0][2]);
+                board[0][2].isRevealed();
+                board[0][2].isClicked = true;
                 break;
             case 4:
-                System.out.println(board[0][3]);
+                board[0][3].isRevealed();
+                board[0][3].isClicked = true;
                 break;
             case 5:
-                System.out.println(board[0][4]);
+                board[0][4].isRevealed();
+                board[0][4].isClicked = true;
                 break;
             case 6:
-                System.out.println(board[1][0]);
+                board[1][0].isRevealed();
+                board[1][0].isClicked = true;
                 break;
             case 7:
-                System.out.println(board[1][1]);
+                board[1][1].isRevealed();
+                board[1][1].isClicked = true;
                 break;
             case 8:
-                System.out.println(board[1][2]);
+                board[1][2].isRevealed();
+                board[1][2].isClicked = true;
                 break;
             case 9:
-                System.out.println(board[1][3]);
+                board[1][3].isRevealed();
+                board[1][3].isClicked = true;
                 break;
             case 10:
-                System.out.println(board[1][4]);
+                board[1][4].isRevealed();
+                board[1][4].isClicked = true;
                 break;
             case 11:
-                System.out.println(board[2][0]);
+                board[2][0].isRevealed();
+                board[2][0].isClicked = true;
                 break;
             case 12:
-                System.out.println(board[2][1]);
+                board[2][1].isRevealed();
+                board[2][1].isClicked = true;
                 break;
             case 13:
-                System.out.println(board[2][2]);
+                board[2][2].isRevealed();
+                board[2][2].isClicked = true;
                 break;
             case 14:
-                System.out.println(board[2][3]);
+                board[2][3].isRevealed();
+                board[2][3].isClicked = true;
                 break;
             case 15:
-                System.out.println(board[2][4]);
+                board[2][4].isRevealed();
+                board[2][4].isClicked = true;
                 break;
             case 16:
-                System.out.println(board[3][0]);
+                board[3][0].isRevealed();
+                board[3][0].isClicked = true;
                 break;
             case 17:
-                System.out.println(board[3][1]);
+                board[3][1].isRevealed();
+                board[3][1].isClicked = true;
                 break;
             case 18:
-                System.out.println(board[3][2]);
+                board[3][2].isRevealed();
+                board[3][2].isClicked = true;
                 break;
             case 19:
-                System.out.println(board[3][3]);
+                board[3][3].isRevealed();
+                board[3][3].isClicked = true;
                 break;
             case 20:
-                System.out.println(board[3][4]);
+                board[3][4].isRevealed();
+                board[3][4].isClicked = true;
                 break;
             case 21:
-                System.out.println(board[4][0]);
+                board[4][0].isRevealed();
+                board[4][0].isClicked = true;
                 break;
             case 22:
-                System.out.println(board[4][1]);
+                board[4][1].isRevealed();
+                board[4][1].isClicked = true;
                 break;
             case 23:
-                System.out.println(board[4][2]);
+                board[4][2].isRevealed();
+                board[4][2].isClicked = true;
                 break;
             case 24:
-                System.out.println(board[4][3]);
+                board[4][3].isRevealed();
+                board[4][3].isClicked = true;
                 break;
             case 25:
-                System.out.println(board[4][4]);
+                board[4][4].isRevealed();
+                board[4][4].isClicked = true;
                 break;
         }
-
-
     }
 
+    public static void main(String[] args){
+        //Thought Process
+        //Make a Game Board where you can detect surroundings
+        //Have Random Bombs
+        //Have a number on a tile based on how many bombs nearby
+        //Tile with revealed and unrevealed state
+
+        // June 19, 2025 To do:
+        // Hide it all first then only show the number of surrounding bombs near it after clicking
 
 
+
+
+        Tile[] tiles = new Tile[25];
+
+        Tile[][] board = new Tile[5][5];
+
+        Random random = new Random();
+        int number;
+        int bombLimit = 5;
+        int bombNumber = 0;
+
+
+        int num = 0;
+
+        //Make the board
+        for(int i=0;i<5;i++){
+            for (int j =0;j<5;j++){
+                number = random.nextInt(0, 2);
+
+                if (number == 1 && bombNumber < bombLimit){
+                    tiles[num] = new Tile(num, false, "Bomb");
+                    bombNumber++;
+                }
+                else{
+                    tiles[num] = new Tile(num, false, "Normal");
+
+                }
+
+                board[i][j] = tiles[num];
+
+                //System.out.println(board[i][j]);
+
+                num = num + 1;
+
+                // maybe make it 2d array like do Tiles[j][i]
+
+            }
+            System.out.println("");
+        }
+
+        //Display the board
+        displayBoard(board);
+
+        for (Tile t : tiles){
+            t.displayInfo();
+        }
+
+        for(int i=0;i<25;i++) {
+            //Ask for player input and actually play the game
+            playerInput(board);
+
+            //Display the board
+            displayBoard(board);
+        }
+    }
 }
