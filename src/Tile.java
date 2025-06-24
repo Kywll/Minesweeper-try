d381d5dff5b9a162a6b96eb7371adfe67638e7e6
Mikell <89644541+Kywll@users.noreply.github.com>
2025-06-24 21:20:57 +0800

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,28 +1,44 @@
 public class Tile {
+
+    // To do:
+    // Make it abstract
+
+
     int boardPosition;
     boolean isClicked;
-    String tileType;
 
 
-
-    Tile(int boardPosition, boolean isClicked, String tileType){
+    Tile(int boardPosition, boolean isClicked){
         this.boardPosition = boardPosition;
         this.isClicked = isClicked;
-        this.tileType = tileType;
+
     }
 
     Tile(){
 
-
     }
 
-    void isRevealed(){
+    public boolean isBomb(){
+        // Checks if a tile is a bomb or not. False on default
+        // You should focus the logic on the function not the tile you are comparing
+        // Like if you're trying to do if(Tile.isBomb()) it would return false if normal and true if bomb
+        // if(!Tile.isBomb()) it would return true if normal and false if bomb
+        // Basically it's like if Normal tile is a bomb then return false obviously
+        // if(!NormalTile.isBomb()) then it returns true since it's like asking if Normal tile is not a bomb which is true
+
+        return false;
+    }
+
+    void revealTile(){
+        // Reveal the tile upon use
         isClicked = true;
-        System.out.println("Tile has been revealed");
+        System.out.println("Tile has been revealed. Is it a bomb?: " + isBomb());
     }
 
+
+    // USED FOR DEBUGGING AND CHECKING:
     void displayInfo(){
-        System.out.println("Board Position: " + boardPosition + ", Is Clicked?: " + isClicked + ", Tile Type: " + tileType);
+        System.out.println("Board Position: " + boardPosition + ", Is Clicked?: " + isClicked + ", Is it a bomb?: " + isBomb());
     }
 
 
