7f2f210eaccae3020551b50ca66e0aef3d86b552
Mikell <89644541+Kywll@users.noreply.github.com>
2025-06-28 12:31:07 +0800

    Add files via upload
    Added a way to make the player choose the size of the board and the number of bombs before starting the game

--- src/src/Game.java
+++ src/src/Game.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 // Handles Player Interaction
 public class Game {
 
+    Board board;
 
-    Board board = new Board(10, 10, 7);
+
+    public void chooseSettings(){
+        // Ask player if they want to flag or not
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Type the Board Height: ");
+        int boardHeight = input.nextInt();
+
+        System.out.print("Type the Board Width: ");
+        int boardWidth = input.nextInt();
+
+        System.out.print("Type the Bomb Limit: ");
+        int bombLimit = input.nextInt();
+
+        // Have to use this.board instead of Board board = new Board() because if you do that you then make another
+        // board instead of just changing the values of the initialized board before in the object
+        this.board = new Board(boardHeight, boardWidth, bombLimit);
+
+    }
 
     public String inputType(){
         // Ask player if they want to flag or not
@@ -53,6 +72,9 @@ public class Game {
     //Tile with revealed and unrevealed state
 
     public void play(){
+        // Choose the setup of the board
+        chooseSettings();
+
         // Create the Board
         board.createBoard();
 
@@ -70,14 +92,19 @@ public class Game {
         //Display the board
             board.displayBoard();
 
+        // Checks if user already used hint so the can't use it again
+        boolean hinted = false;
         // Actually play the game
         for(int i=1;i<(board.boardHeight * board.boardWidth);i++) {
             // Check if you've won
             String winLoseStatus = board.winLose();
             if (!winLoseStatus.equals("Win") && !winLoseStatus.equals("Lose")){
                 // Asks the user if they want to get a hint
-                if (inputHint().equals("yes")){
-                    board.hint();
+                if(!hinted){
+                    if (inputHint().equals("yes")){
+                        board.hint();
+                        hinted = true;
+                    }
                 } else{
                     // Asks the user if they want to flag or not each iteration
                     String flagOrReveal = inputType();
